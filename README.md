9fb697b4f053c877d3d5eaf280d55a23476ace1d
muratcanclb <muratcan.celebi@outlook.com>
2024-04-17 22:30:17 +0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # roomo.api
 Otel yönetim sistemi
+<img width="500px" height="200px" src="./roomo.api/img/getRequest.png" /> 
+<img width="500px" height="200px" src="./roomo.api/img/requestAll.png" /> 
--- roomo.api/obj/Debug/net8.0/roomo.api.AssemblyInfo.cs
+++ roomo.api/obj/Debug/net8.0/roomo.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("roomo.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c208639be46e8e2fa55c6a33e348c0bc9df0ec2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6c240397b302e739dae4a4d3b0da7b4a9d4522")]
 [assembly: System.Reflection.AssemblyProductAttribute("roomo.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("roomo.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
